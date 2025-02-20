7e0310fdeeffe06b7e2833a4c8ce0800d5c87143
pratikshaMM <pratiksha.magar@zemosolabs.com>
2025-02-20 17:08:00 +0530

    Update main.tf

--- code/02-one-server/main.tf
+++ code/02-one-server/main.tf
@@ -6,7 +6,7 @@ provider "google" {
 
 # Create a Google Compute instance
 resource "google_compute_instance" "example" {
-  name          = "example_vm"
+  name          = "example-vm"
   machine_type  = "f1-micro"
   zone          = "us-central1-c"
   
